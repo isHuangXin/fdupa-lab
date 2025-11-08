36f6599e118ed6c3ec2f16c3eead5f3e79349a0c
Xin Huang <huangxin190@huawei.com>
2025-11-09 02:20:17 +0800

    【新增代码Lab1】完成第三版_优化代码结构_Precision72_Recall100

--- lib/analysis/intervalAnalysis.cpp
+++ lib/analysis/intervalAnalysis.cpp
@@ -23,148 +23,29 @@ void IntervalAnalysis::fixedPoint() {
 	if (!insts.empty()) inputStates[insts[0]->getLabel()] = initState;
 
 	while (!worklist.empty()) worklist.pop();
-	worklist.push(insts.empty() ? 0 : insts[0]->getLabel());
+	if (!insts.empty()) worklist.push(insts[0]->getLabel());
 
-	std::unordered_map<size_t, bool> inQueue;
-	size_t entryLabel = insts.empty() ? 0 : insts[0]->getLabel();
-	inQueue[entryLabel] = true;
-
+	// simple driver: compute transfer and delegate successor handling
 	while (!worklist.empty()) {
 		size_t label = worklist.front();
 		worklist.pop();
-	inQueue[label] = false;
 
-		// find inst by label
+		// find inst by label (safe lookup)
 		IR::Inst *inst = nullptr;
 		if (label < insts.size() && insts[label] && insts[label]->getLabel() == label) {
 			inst = insts[label];
 		} else {
 			for (auto cand : insts) {
-				if (cand && cand->getLabel() == label) {
-					inst = cand;
-					break;
-				}
+				if (cand && cand->getLabel() == label) { inst = cand; break; }
 			}
 		}
 		if (!inst) continue;
 
 		States &inState = inputStates[label];
-
-		// If instruction: handle branches with constraint filtering
-		if (inst->getInstType() == IR::InstType::IfInst) {
-			IR::IfInst *ifInst = (IR::IfInst *)inst;
-			IR::Value *left = ifInst->getOperand(0);
-			IR::Value *right = ifInst->getOperand(1);
-			std::string varName = left->getAsVariable();
-			long long c = right->getAsNumber();
-			IR::CmpOperator op = ifInst->getCmpOperator();
-
-			auto restrictState = [&](const States &s, bool takeTrue, States &outState) -> bool {
-				outState = s;
-				Interval old;
-				auto it = s.find(varName);
-				if (it == s.end() || it->second.isBottom) {
-					// if unknown, assume full range
-					old.isBottom = false;
-					old.l = 0;
-					old.r = 255;
-				} else {
-					old = it->second;
-				}
-
-				long long nl = old.l;
-				long long nr = old.r;
-				long long cl = 0, cr = 255;
-
-				if (takeTrue) {
-					switch (op) {
-					case IR::CmpOperator::EQ:
-						cl = c; cr = c; break;
-					case IR::CmpOperator::GT:
-						cl = c + 1; cr = 255; break;
-					case IR::CmpOperator::GEQ:
-						cl = c; cr = 255; break;
-					case IR::CmpOperator::LT:
-						cl = 0; cr = c - 1; break;
-					case IR::CmpOperator::LEQ:
-						cl = 0; cr = c; break;
-					default:
-						cl = 0; cr = 255; break;
-					}
-				} else {
-					// false branch (complement). For EQ complement is non-contiguous; approximate conservatively
-					switch (op) {
-					case IR::CmpOperator::EQ:
-						cl = 0; cr = 255; break;
-					case IR::CmpOperator::GT:
-						// !(x>c) => x <= c
-						cl = 0; cr = c; break;
-					case IR::CmpOperator::GEQ:
-						// !(x>=c) => x < c
-						cl = 0; cr = c - 1; break;
-					case IR::CmpOperator::LT:
-						// !(x<c) => x >= c
-						cl = c; cr = 255; break;
-					case IR::CmpOperator::LEQ:
-						// !(x<=c) => x > c
-						cl = c + 1; cr = 255; break;
-					default:
-						cl = 0; cr = 255; break;
-					}
-				}
-
-				if (cl < 0) cl = 0;
-				if (cr > 255) cr = 255;
-
-				long long newL = std::max(nl, cl);
-				long long newR = std::min(nr, cr);
-				if (newL > newR) return false; // branch unreachable under this inState
-
-				Interval newIv;
-				newIv.isBottom = false;
-				newIv.l = newL;
-				newIv.r = newR;
-				outState[varName] = newIv;
-				return true;
-			};
-
-			const auto &sucs = inst->getSuccessors();
-			// false branch -> successor[0], true branch -> successor[1]
-			if (sucs.size() >= 1) {
-				States ns;
-				if (restrictState(inState, false, ns)) {
-					size_t succ = sucs[0]->getLabel();
-					if (joinInto(ns, inputStates[succ]) && !inQueue[succ]) {
-						worklist.push(succ);
-						inQueue[succ] = true;
-					}
-				}
-			}
-			if (sucs.size() >= 2) {
-				States ns;
-				if (restrictState(inState, true, ns)) {
-					size_t succ = sucs[1]->getLabel();
-					if (joinInto(ns, inputStates[succ]) && !inQueue[succ]) {
-						worklist.push(succ);
-						inQueue[succ] = true;
-					}
-				}
-			}
-
-			continue;
-		}
-
-		// non-if inst: normal transfer and push successors
 		States outState = transfer(inst, inState);
-		for (auto sucInst : inst->getSuccessors()) {
-			size_t succ = sucInst->getLabel();
-			if (joinInto(outState, inputStates[succ]) && !inQueue[succ]) {
-				worklist.push(succ);
-				inQueue[succ] = true;
-			}
+		addSuccessors(label, outState);
 		}
 	}
-}
 
 /****************************************************************
 ********************* Your code starts here *********************
@@ -334,7 +215,7 @@ bool IntervalAnalysis::joinInto(const States &outputState, States &sucInputState
 }
 
 void IntervalAnalysis::addSuccessors(size_t nowLabel, States outputState) {
-	// find inst for nowLabel
+	// find inst for nowLabel (safe lookup)
 	IR::Inst *inst = nullptr;
 	if (nowLabel < insts.size() && insts[nowLabel] && insts[nowLabel]->getLabel() == nowLabel)
 		inst = insts[nowLabel];
@@ -343,6 +224,111 @@ void IntervalAnalysis::addSuccessors(size_t nowLabel, States outputState) {
 	}
 	if (!inst) return;
 
+	// use input state for branch filtering
+	States inState = inputStates[nowLabel];
+
+	// If instruction: handle branches with constraint filtering
+	if (inst->getInstType() == IR::InstType::IfInst) {
+		IR::IfInst *ifInst = (IR::IfInst *)inst;
+		IR::Value *left = ifInst->getOperand(0);
+		IR::Value *right = ifInst->getOperand(1);
+		std::string varName = left->getAsVariable();
+		long long c = right->getAsNumber();
+		IR::CmpOperator op = ifInst->getCmpOperator();
+
+		auto restrictState = [&](const States &s, bool takeTrue, States &outState) -> bool {
+			outState = s;
+			Interval old;
+			auto it = s.find(varName);
+			if (it == s.end() || it->second.isBottom) { old.isBottom = false; old.l = 0; old.r = 255; }
+			else old = it->second;
+
+			long long nl = old.l;
+			long long nr = old.r;
+			long long cl = 0, cr = 255;
+			if (takeTrue) {
+				switch (op) {
+				case IR::CmpOperator::EQ: cl = c; cr = c; break;
+				case IR::CmpOperator::GT: cl = c + 1; cr = 255; break;
+				case IR::CmpOperator::GEQ: cl = c; cr = 255; break;
+				case IR::CmpOperator::LT: cl = 0; cr = c - 1; break;
+				case IR::CmpOperator::LEQ: cl = 0; cr = c; break;
+				default: cl = 0; cr = 255; break;
+				}
+			} else {
+				switch (op) {
+				case IR::CmpOperator::EQ: cl = 0; cr = 255; break;
+				case IR::CmpOperator::GT: cl = 0; cr = c; break;
+				case IR::CmpOperator::GEQ: cl = 0; cr = c - 1; break;
+				case IR::CmpOperator::LT: cl = c; cr = 255; break;
+				case IR::CmpOperator::LEQ: cl = c + 1; cr = 255; break;
+				default: cl = 0; cr = 255; break;
+				}
+			}
+			if (cl < 0) cl = 0;
+			if (cr > 255) cr = 255;
+			long long newL = std::max(nl, cl);
+			long long newR = std::min(nr, cr);
+			if (newL > newR) return false;
+			Interval newIv; newIv.isBottom = false; newIv.l = newL; newIv.r = newR;
+			outState[varName] = newIv;
+			return true;
+		};
+
+		auto intersectRange = [&](const States &s, long long cl, long long cr, States &outState) -> bool {
+			outState = s;
+			Interval old;
+			auto it = s.find(varName);
+			if (it == s.end() || it->second.isBottom) { old.isBottom = false; old.l = 0; old.r = 255; }
+			else old = it->second;
+			if (cl < 0) cl = 0;
+			if (cr > 255) cr = 255;
+			long long newL = std::max((long long)old.l, cl);
+			long long newR = std::min((long long)old.r, cr);
+			if (newL > newR) return false;
+			Interval newIv; newIv.isBottom = false; newIv.l = newL; newIv.r = newR;
+			outState[varName] = newIv;
+			return true;
+		};
+
+		const auto &sucs = inst->getSuccessors();
+		// false successor (index 0)
+		if (sucs.size() >= 1) {
+			size_t succLabel = sucs[0]->getLabel();
+			if (op == IR::CmpOperator::EQ) {
+				if (c - 1 >= 0) {
+					States ns1;
+					if (intersectRange(inState, 0, c - 1, ns1)) {
+						if (joinInto(ns1, inputStates[succLabel])) worklist.push(succLabel);
+					}
+				}
+				if (c + 1 <= 255) {
+					States ns2;
+					if (intersectRange(inState, c + 1, 255, ns2)) {
+						if (joinInto(ns2, inputStates[succLabel])) worklist.push(succLabel);
+					}
+				}
+			} else {
+				States ns;
+				if (restrictState(inState, false, ns)) {
+					if (joinInto(ns, inputStates[succLabel])) worklist.push(succLabel);
+				}
+			}
+		}
+
+		// true successor (index 1)
+		if (sucs.size() >= 2) {
+			size_t succLabel = sucs[1]->getLabel();
+			States ns;
+			if (restrictState(inState, true, ns)) {
+				if (joinInto(ns, inputStates[succLabel])) worklist.push(succLabel);
+			}
+		}
+
+		return;
+	}
+
+	// non-if inst: normal transfer and push successors
 	for (auto suc : inst->getSuccessors()) {
 		if (!suc) continue;
 		size_t lab = suc->getLabel();
